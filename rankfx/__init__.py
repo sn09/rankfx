1b1c032376743399fcd69e20780f5e0bfa1dbf20
Saraev Nikita <saraevnik0909@gmail.com>
2025-05-20 12:21:23 +0300

    fix: setup.py

--- rankfx/__init__.py
+++ rankfx/__init__.py
@@ -1,5 +1,3 @@
 """Package containing main framework logic."""
 
-from . import common, dcnv2, finalnet
-
-__all__ = ["common", "dcnv2", "finalnet"]
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 """Setup file for package."""
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 
 def readme():
@@ -11,14 +11,14 @@ def readme():
 
 setup(
   name="rankfx",
-  version="1.0.0",
+  version="0.1.0",
   author="saraevn",
   author_email="saraevnik0909@gmail.com",
   description="Python package for working with NN models",
   long_description=readme(),
   long_description_content_type="text/markdown",
   url="https://github.com/sn09/rankfx",
-  packages=["rankfx"],
+  packages=find_packages(exclude=["notebooks"]),
   install_requires=[
     "torch>=2.2.1",
     "pydantic>=2.10.6",
